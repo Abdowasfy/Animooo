103e6b7f73b4eda90ae79ba027a5a8f3c95389e0
Abdelrahman <wasfyabdo215@gmail.com>
2026-07-25 00:57:25 +0300

    Update Animooo UI

--- lib/pages/views/new_password.dart
+++ lib/pages/views/new_password.dart
@@ -30,94 +30,96 @@ class _NewPasswordState extends State<NewPassword> {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Icon(
-                      Icons.arrow_back_ios_new,
-                      size: 18.sp,
-                      color: AppColors.primary,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.arrow_back_ios_new,
+                        size: 18.sp,
+                        color: AppColors.primary,
+                      ),
                     ),
+                    Gap(4.w),
+                    Text(
+                      "Cancel",
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                  ],
+                ),
+
+                Gap(24.h),
+
+                Text(
+                  "Create New Password",
+                  style: TextStyle(
+                    fontSize: 22.sp,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.primary,
                   ),
-                  Gap(4.w),
-                  Text(
-                    "Cancel",
+                ),
+
+                Gap(10.h),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "New Password",
                     style: TextStyle(
-                      fontSize: 18.sp,
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.primary,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
-                ],
-              ),
-
-              Gap(24.h),
-
-              Text(
-                "Create New Password",
-                style: TextStyle(
-                  fontSize: 22.sp,
-                  fontWeight: FontWeight.bold,
-                  color: AppColors.primary,
                 ),
-              ),
 
-              Gap(10.h),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "New Password",
-                  style: TextStyle(
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.w500,
+                Gap(8.h),
+
+                CustomTextField(
+                  controller: pass,
+                  hint: "********",
+                  isPassword: true,
+                ),
+                Gap(15),
+                PasswordValidation(
+                  hasMinLength: true,
+                  hasUpperCase: false,
+                  hasLowerCase: true,
+                  hasSpecialChar: true,
+                  hasNumber: true,
+                ),
+                Gap(20),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Confirm Password",
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
-              ),
 
-              Gap(8.h),
+                Gap(8.h),
 
-              CustomTextField(
-                controller: pass,
-                hint: "********",
-                isPassword: true,
-              ),
-              Gap(15),
-              PasswordValidation(
-                hasMinLength: true,
-                hasUpperCase: false,
-                hasLowerCase: true,
-                hasSpecialChar: true,
-                hasNumber: true,
-              ),
-              Gap(20),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Confirm Password",
-                  style: TextStyle(
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.w500,
-                  ),
+                CustomTextField(
+                  controller: conpass,
+                  hint: "********",
+                  isPassword: true,
                 ),
-              ),
+                Gap(100),
 
-              Gap(8.h),
-
-              CustomTextField(
-                controller: conpass,
-                hint: "********",
-                isPassword: true,
-              ),
-              Gap(100),
-
-              CustomButton(text: "Submit", onPressed: () {}),
-            ],
+                CustomButton(text: "Submit", onPressed: () {}),
+              ],
+            ),
           ),
         ),
       ),
